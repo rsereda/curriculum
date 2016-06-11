ef7839df867f165ea99afe9658e642c804effed1
Christoph <staudtc@live.de>
2016-06-11 14:29:37 +0200

    Update

--- modules/backend/widgets/Lists.php
+++ modules/backend/widgets/Lists.php
@@ -308,13 +308,6 @@ class Lists extends WidgetBase
     {
         return str_replace('@', $table.'.', $sql);
     }
-    
-    protected function castForPostgres($columnName, $column){
-      if($this->model->getConnection() instanceof \Illuminate\Database\PostgresConnection  && $column->type == 'number'){
-        return 'CAST('. $columnName .' as TEXT)';
-      }
-      return $columnName;
-    }
 
     /**
      * Applies any filters to the model.
@@ -359,7 +352,7 @@ class Lists extends WidgetBase
                 else {
                     $columnName = isset($column->sqlSelect)
                         ? DbDongle::raw($this->parseTableName($column->sqlSelect, $primaryTable))
-                        : $this->castForPostgres(Db::getTablePrefix() . $primaryTable . '.' . $column->columnName, $column);
+                        : Db::getTablePrefix() . $primaryTable . '.' . $column->columnName;
 
                     $primarySearchable[] = $columnName;
                 }
