ecff3d76a210a81dde38fa301b0a0d4b7a534657
Christoph <staudtc@live.de>
2016-09-28 17:06:34 +0200

    changes at available course

--- plugins/kironuniversity/curriculum/controllers/AvailableCourses.php
+++ plugins/kironuniversity/curriculum/controllers/AvailableCourses.php
@@ -72,6 +72,87 @@ class AvailableCourses extends Controller
     }
   }
 
+  private function multiRequest($data, $options = array()) {
+
+    // array of curl handles
+    $curly = array();
+    // data to be returned
+    $result = array();
+
+    // multi handle
+    $mh = curl_multi_init();
+
+    // loop through $data and create curl handles
+    // then add them to the multi-handle
+    foreach ($data as $id => $d) {
+
+      $curly[$id] = curl_init();
+
+      $url = (is_array($d) && !empty($d['url'])) ? $d['url'] : $d;
+      curl_setopt($curly[$id], CURLOPT_URL,            $url);
+      curl_setopt($curly[$id], CURLOPT_HEADER,         0);
+      curl_setopt($curly[$id], CURLOPT_RETURNTRANSFER, 1);
+
+      // post?
+      if (is_array($d)) {
+        if (!empty($d['post'])) {
+          curl_setopt($curly[$id], CURLOPT_POST,       1);
+          curl_setopt($curly[$id], CURLOPT_POSTFIELDS, $d['post']);
+        }
+      }
+
+      // extra options?
+      if (!empty($options)) {
+        curl_setopt_array($curly[$id], $options);
+      }
+
+      curl_multi_add_handle($mh, $curly[$id]);
+    }
+
+    // execute the handles
+    $running = null;
+    do {
+      curl_multi_exec($mh, $running);
+    } while($running > 0);
+
+
+    // get content and remove handles
+    foreach($curly as $id => $c) {
+      $result[$id] = curl_multi_getcontent($c);
+      curl_multi_remove_handle($mh, $c);
+    }
+
+    // all done
+    curl_multi_close($mh);
+
+    return $result;
+  }
+
+  public function scrapeMammooc(){
+    $urls = [];
+    for($i=1;$i<=2; $i++){
+      $urls[] = 'https://mammooc.org/courses.json?page='.$i;
+    }
+
+    $chunked = array_chunk($urls,30);
+    $combined = '';
+    $count = 0;
+    foreach($chunked as $chunk){
+      $results = $this->multiRequest($chunk);
+      foreach($results as $result){
+        if($result ==  '[]'){
+          break 2;
+        }
+        $combined .= trim($result, '[]').',';
+        $count++;
+      }
+    }
+    $combined = '['.trim($combined,',').']';
+    $data = json_decode($combined, true);
+    dd($count, $data[0]);
+
+  }
+
   public function scrapeCourses() {
     //TODO: Change this to some kiron-hosted endpoint or access the class-central db directly.
     $courseSourceUrl = 'http://class-central.wailord.lb2.eu/web/api.php';
